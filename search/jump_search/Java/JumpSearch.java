dca5d45b6cfcc9ad88ac060c4ba718241cffc94a
Marshal J. Dickey <mdickey14@gmail.com>
2018-10-03 19:27:41 -0400

    minor update

--- search/jump_search/Java/JumpSearch.java
+++ search/jump_search/Java/JumpSearch.java
@@ -13,7 +13,7 @@ public class JumpSearch {
      *          The int to look for
      * @return The index of the key ({@link #NOT_FOUND} if the key was not found).
      */
-    int search(int[] array, int key) {
+    static int search(int[] array, int key) {
 
         // Determine the block size
         int blockSize = (int) Math.sqrt(array.length);
@@ -42,7 +42,7 @@ public class JumpSearch {
         return NOT_FOUND;
     }
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
         int[] test = { 1, 2, 3, 20, 60, 66, 77, 78, 100, 120, 123 };
         int key = 77;
         int indexOfKey = search(test, key);
