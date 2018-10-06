fa47c235cff868ff61d75e138fe896d42d50966a
Rajashree Parhi <rajashreeparhi23@gmail.com>
2018-10-06 22:22:21 +0530

    Optimised the existing C++ solution

--- sort/bubble_sort/C++/bubble_sort.cpp
+++ sort/bubble_sort/C++/bubble_sort.cpp
@@ -1,34 +1,22 @@
-#include<iostream.h>
-#include<conio.h>
-void main()
+#include<iostream>
+using namespace std;
+int main()
 {
-	clrscr();
-	int n, i, arr[50], j, temp;
-	cout<<"Enter total number of elements :";
+	long long n,a[100000],temp;
 	cin>>n;
-	cout<<"Enter "<<n<<" numbers :";
-	for(i=0; i<n; i++)
+	for(int i=0;i<n;i++)
+	  cin>>a[i];
+	for(int i=n-1;i>0;i--)
 	{
-		cin>>arr[i];
+		 for(int j=0;j<i;j++)
+		 if(a[j]>a[j+1])
+		 {
+			 temp=a[j];
+			 a[j]=a[j+1];
+			 a[j+1]=temp;
+		 }
 	}
-	cout<<"Sorting array using bubble sort technique...\n";
-	for(i=0; i<(n-1); i++)
-	{
-		for(j=0; j<(n-i-1); j++)
-		{
-			if(arr[j]>arr[j+1])
-			{
-				temp=arr[j];
-				arr[j]=arr[j+1];
-				arr[j+1]=temp;
-			}
-		}
-	}
-	cout<<"Elements sorted successfully..!!\n";
-	cout<<"Sorted list in ascending order :\n";
-	for(i=0; i<n; i++)
-	{
-		cout<<arr[i]<<" ";
-	}
-	getch();
+	for(int i=0;i<n;i++)
+	   cout<<a[i]<<' ';
+return 0;
 }
