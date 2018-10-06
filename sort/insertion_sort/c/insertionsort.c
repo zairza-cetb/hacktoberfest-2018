da439f6aae3c118fbd32af34077c801a5a665c9d
Shivam Kumar <shivamkr1107@gmail.com>
2018-10-07 00:31:05 +0530

    insertion Sort.c

--- sort/insertion_sort/c/insertionsort.c
+++ sort/insertion_sort/c/insertionsort.c
@@ -1,35 +1,47 @@
-#include <stdio.h>
-
-void insertionsort(int arr[], int n)
-{
-  int i, j, temp;
-
-  for(i = 1; i < n; i++){
-    temp = arr[i];
-    for(j = i; j > 0 && arr[j-1] > temp; j--){
-      arr[j] = arr[j-1];
-    }
-    arr[j] = temp;
-  }
-}
-
-int main()
-{
-  int i;
-  int num[] = { 3, 2, 5, 8, 1, 6, 7, 4 }; 
-  const int n = 8;
-
-  for(i = 0; i < n; i++){
-    printf("%d ", num[i]);
-  }
-  printf("\n");
-
-  insertionsort(num, n);
-
-  for(i = 0; i < n; i++){
-    printf("%d ", num[i]);
-  }
-  printf("\n");
-
-  return 0;
-}
+// C program for implementation of selection sort 
+#include <stdio.h> 
+  
+void swap(int *xp, int *yp) 
+{ 
+    int temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
+} 
+  
+void selectionSort(int arr[], int n) 
+{ 
+    int i, j, min_idx; 
+  
+    // One by one move boundary of unsorted subarray 
+    for (i = 0; i < n-1; i++) 
+    { 
+        // Find the minimum element in unsorted array 
+        min_idx = i; 
+        for (j = i+1; j < n; j++) 
+          if (arr[j] < arr[min_idx]) 
+            min_idx = j; 
+  
+        // Swap the found minimum element with the first element 
+        swap(&arr[min_idx], &arr[i]); 
+    } 
+} 
+  
+/* Function to print an array */
+void printArray(int arr[], int size) 
+{ 
+    int i; 
+    for (i=0; i < size; i++) 
+        printf("%d ", arr[i]); 
+    printf("\n"); 
+} 
+  
+// Driver program to test above functions 
+int main() 
+{ 
+    int arr[] = {64, 25, 12, 22, 11}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    selectionSort(arr, n); 
+    printf("Sorted array: \n"); 
+    printArray(arr, n); 
+    return 0; 
+} 
