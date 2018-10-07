b32450c19c7d68888310b13b001ef370d7e2b451
Alexander Mykolaichuk <alexmix3000@gmail.com>
2018-10-07 12:00:27 +0300

    linear search js

---
+++ search/linear_search/javascript/linearsearch.js
@@ -0,0 +1,8 @@
+const linearSearch = (array, toFind) => {
+        for(let i = 0; i < array.length; i++){
+          if(array[i] === toFind) return i;
+        }
+        return -1;
+      }
+
+export default linearSearch;
